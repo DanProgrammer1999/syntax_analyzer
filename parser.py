ad91117f8021999a91f8b896d688dde6650cd5b1
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2019-09-21 05:05:21 +0300

    fix(parser): some (not all) bugs

--- parser.py
+++ parser.py
@@ -8,6 +8,12 @@ class AstNode:
         self.parent = None
         self.children = None
 
+    def __repr__(self):
+        s = str(self.__token.type)
+        if self.__token.type is TokenType.Literal:
+            s += ": {}".format(self.__token.value)
+
+        return s
 
 class Parser:
     def __init__(self, filename):
@@ -30,7 +36,9 @@ class Parser:
         expected_types = (TokenType.OpEquals, TokenType.OpLessThan, TokenType.OpMoreThan)
 
         if parent.type not in expected_types:
-            raise UnexpectedTokenException(self.filename, "expected either of these symbols: \'>\', \'<\', or \'=\'")
+            self.__lexer.push_back(parent)
+            return left
+            # raise UnexpectedTokenException(self.filename, "expected either of these symbols: \'>\', \'<\', or \'=\'")
 
         right = self.__parse_term()
 
@@ -38,33 +46,36 @@ class Parser:
 
     def __parse_term(self):
         left = self.__parse_factor()
+        parent = self.__lexer.get()
 
-        while True:
-            parent = self.__lexer.get()
-
-            if not parent:
-                return left
+        while parent:
 
             expected_types = (TokenType.OpPlus, TokenType.OpMinus)
             if parent.type not in expected_types:
                 self.__lexer.push_back(parent)
-                return left
+                break
 
             right = self.__parse_factor()
-
             left = self.__make_binary_tree(AstNode(parent), left, right)
+            parent = self.__lexer.get()
+
+        return left
 
     def __parse_factor(self):
 
         left = self.__parse_primary()
-        while True:
-            parent = self.__lexer.get()
-            if not parent:
-                return left
+        parent = self.__lexer.get()
+        while parent:
 
             if parent.type is not TokenType.OpMultiply:
                 self.__lexer.push_back(parent)
-                return left
+                break
+
+            right = self.__parse_primary()
+            left = self.__make_binary_tree(AstNode(parent), left, right)
+            parent = self.__lexer.get()
+
+        return left
 
     def __parse_primary(self):
         left = self.__lexer.get()
@@ -73,10 +84,10 @@ class Parser:
 
             if self.__lexer.get().type is not TokenType.RightParen:
                 raise UnexpectedTokenException(self.filename, "missing closing parenthesis")
+            return left
 
         elif left.type is not TokenType.Literal:
             raise UnexpectedTokenException(self.filename, "malformed expression: expected integer or parenthesis")
-
         return AstNode(left)
 
     @staticmethod
@@ -86,6 +97,20 @@ class Parser:
         child1.parent, child2.parent = parent, parent
         return parent
 
+    def __repr__(self):
+        curr_nodes = [self.tree]
+        res = ""
+        while curr_nodes:
+            new_nodes = []
+            for node in curr_nodes:
+                res += "{" + str(node) + "}\t"
+
+                if node.children:
+                    new_nodes += node.children
+            res += "\n"
+            curr_nodes = new_nodes
+
+        return res
 
 class Error(Exception):
     def __init__(self, value):
@@ -103,8 +128,3 @@ class UnexpectedTokenException(Error):
 
         if text:
             self.value += ": {}".format(text)
-
-
-if __name__ == "__main__":
-    p = Parser("test.m")
-    print(p.tree)
