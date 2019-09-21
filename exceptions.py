097c21d09d0c6a7d7811349e485968b2284e1509
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2019-09-21 16:10:40 +0300

    Provided better exception structure

--- exceptions.py
+++ exceptions.py
@@ -6,20 +6,16 @@ class Error(Exception):
         return repr(self.value)
 
 
-class LexicalException(Error):
-    def __init__(self, filename=None, position: tuple = None, custom_text=None):
-        text = "Lexical Exception"
-        if filename:
-            text += " in {}".format(filename)
-        if position:
-            text += " at line {}, position {}".format(position[0], position[1])
-        if custom_text:
-            text += ": {}".format(custom_text)
-        text += '.'
-        self.value = text
+class SyntaxError(Error):
+    def __init__(self, filename, position, text=None):
+        self.value = "Syntax error in file {} at position <{}:{}>".format(filename, position[0], position[1])
+        if text:
+            self.value += ": {}".format(text)
+
+        self.value += "."
 
 
-class IllegalCharacterException(LexicalException):
+class IllegalCharacterException(SyntaxError):
     def __init__(self, filename=None, position=None, character=None):
         text = "illegal character"
         if character:
@@ -27,7 +23,7 @@ class IllegalCharacterException(LexicalException):
         super().__init__(filename, position, text)
 
 
-class InvalidNumberFormatException(LexicalException):
+class InvalidNumberFormatException(SyntaxError):
     def __init__(self, filename=None, position=None, number=None):
         text = "bad number format: number{}starts with 0".format(" {} ".format(number) if number else " ")
         super().__init__(filename, position, text)
